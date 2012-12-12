6fd3bf7b0bf213fb944dff76ca6016ab09447590
Ells Campbell <ells.cubed@gmail.com>
2012-12-12 18:55:52 -0500

    added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,17 @@ describe "Static pages" do
                     :text => "Flu App | About")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Flu App | Contact Us")
+    end
+  end
 end
