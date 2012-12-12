a45f8d612f608738f3ecb367788f68aac369a1ec
Ells Campbell <ells.cubed@gmail.com>
2012-12-12 18:05:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FluApp::Application.config.secret_token = 'cebda10aa99acc6fba1b18eab9e9052e6d9a1eaa15e03363264b775ff294c28e55ae737ce1b705c15532336f250f58aa97bb577c842917d366d925036791d0b1'
