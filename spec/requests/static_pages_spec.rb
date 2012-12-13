890d361d4a95ccfcf9a46fa8ab6a65723552fa57
Ells Campbell <ells.cubed@gmail.com>
2012-12-13 15:38:16 -0500

    Finished layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,57 +2,30 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path } 
 
-    it "should have the h1 'Flu App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Flu App')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Flu App | Home")
-    end
+    it { should have_selector('h1', text: 'Flu App') }
   end
 
   describe "Help page" do
+    before {visit help_path}
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Flu App | Help")
-    end
+    it { should have_selector('h1', text: 'Help')}
   end
 
   describe "About page" do
+    before {visit about_path}
 
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
-
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "Flu App | About")
-    end
+    it { should have_selector('h1', text: 'About')}
   end
-
+  
   describe "Contact page" do
+    before {visit contact_path}
 
-    it "should have the h1 'Contact Us'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact Us')
-    end
-
-    it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Flu App | Contact Us")
-    end
+    it { should have_selector('h1', text: 'Contact Us')}
   end
+
 end
