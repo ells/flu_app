1a072cd58bda3c9c4f6e68bc8692f9d622beea63
Ells Campbell <ells.cubed@gmail.com>
2012-12-12 19:52:56 -0500

    Revert "advanced setup"
    This reverts commit 88796a2c95e2266f910667fc937a3069d811fd02.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Flu App" }
-
   describe "Home page" do
 
     it "should have the h1 'Flu App'" do
@@ -13,7 +11,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "Flu App | Home")
     end
   end
 
@@ -27,7 +25,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "#{base_title} | Help")
+                        :text => "Flu App | Help")
     end
   end
 
@@ -41,7 +39,7 @@ describe "Static pages" do
     it "should have the title 'About'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "#{base_title} | About")
+                    :text => "Flu App | About")
     end
   end
 
@@ -54,7 +52,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => "#{base_title} | Contact Us")
+      page.should have_selector('title', :text => "Flu App | Contact Us")
     end
   end
 end
