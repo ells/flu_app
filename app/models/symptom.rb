2ee992055c79b8ab8c5ee1ba842ad1973c8dde23
Ells Campbell <ells.cubed@gmail.com>
2013-01-16 14:52:21 -0500

    proper association between disease:symptom:metric
    deletion, rejection if empty, etc

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -2,5 +2,5 @@ class Symptom < ActiveRecord::Base
   attr_accessible :name, :metrics, :metrics_attributes
   has_many :metrics, as: :metricizable
   belongs_to :symptomable, polymorphic: true
-  accepts_nested_attributes_for :metrics
+  accepts_nested_attributes_for :metrics, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy=>true
 end
