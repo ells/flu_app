970230b8ed322593d8f3c803d2274117af9b738a
Ells Campbell <ells.cubed@gmail.com>
2013-02-05 14:48:02 -0500

    bare minimum symptom submission working

--- app/models/symptom_set.rb
+++
@@ -1,6 +0,0 @@
-class SymptomSet < ActiveRecord::Base
-  attr_accessible :symptoms, :symptoms_attributes
-  belongs_to :user
-  has_many :symptoms, :dependent => :destroy
-  accepts_nested_attributes_for :symptoms, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy=>true
-end
