0ccb88a6cc89d9208b07ccd2ed5d63b7728e19e1
Ells Campbell <ells.cubed@gmail.com>
2013-02-05 18:17:08 -0500

    basic symptom_set tests

--- spec/models/symptom_set_spec.rb
+++ spec/models/symptom_set_spec.rb
@@ -1,7 +1,13 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe SymptomSet do
-  it "should be valid" do
-    SymptomSet.new.should be_valid
-  end
+  let(:user) {FactoryGirl.create(:user) }
+  before {@symptom_set = SymptomSet.new()}
+  
+  
+  subject {@symptom_set}
+  
+  it {should respond_to(:user_id)}
+  it {should respond_to(:user)}  
+ 
 end
